7ccc4f29b39d6a0a11f400df2e2d59a86cf9135b
airecom <aire.com@gmail.com>
2015-07-27 14:49:03 +0200

    Comienzo
    Comienzo

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+var quizController = require('../controller/quiz_controller');
+var infoController = require('../controller/info_controller');
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Quiz' });
+});
+
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load);  // autoload :quizId
+
+//Definición de rutas auxiliares
+router.get('/author',   infoController.author);
+
+//Definición de rutas de /quizes
+router.get('/quizes',                      quizController.index);
+router.get('/quizes/:quizId(\\d+)',        quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+
+module.exports = router;
