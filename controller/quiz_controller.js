0618f9d959b014872fe16344f436c1398db5d9a6
airecom <aire.com@gmail.com>
2015-07-31 14:38:02 +0200

    Crear Comentario (2)

--- controller/quiz_controller.js
+++ controller/quiz_controller.js
@@ -2,7 +2,7 @@ var models = require('../models/models.js');
 
 // Autoload - factoriza el cÃ³digo si ruta incluye :quizId
 exports.load = function(req, res, next, quizId) {
-    models.Quiz.find({
+  models.Quiz.find({
             where: {
                 id: Number(quizId)
             },
@@ -10,11 +10,12 @@ exports.load = function(req, res, next, quizId) {
                 model: models.Comment
             }]
         }).then(function(quiz) {
+      if (quiz) {
         req.quiz = quiz;
         next();
-      } else { next(new Error('No existe quizId=' + quizId)); }
+      } else{next(new Error('No existe quizId=' + quizId))}
     }
-  ).catch(function(error) { next(error);});
+  ).catch(function(error){next(error)});
 };
 
 // GET /quizes
