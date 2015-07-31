6897a6d6984e0ea752b1fa9383b3c36a620a36f5
airecom <aire.com@gmail.com>
2015-07-31 23:07:31 +0200

    Moderación de comentarios (4)

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -25,7 +25,7 @@ exports.publish = function(req, res) {
   req.comment.publicado = true;
 
   req.comment.save( {fields: ["publicado"]})
-    .then( function(){ res.redirect('/quizes/'+req.quizId);} )
+    .then( function(){ res.redirect('/quizes/'+req.params.quizId);} )
     .catch(function(error){next(error)});
 
   };
--- routes/index.js
+++ routes/index.js
@@ -36,6 +36,6 @@ router.delete('/quizes/:quizId(\\d+)',     sessionController.loginRequired, quiz
 // Definición de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',    commentController.create);
-router.put('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', 
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', 
 	sessionController.loginRequired, commentController.publish);
 module.exports = router;
